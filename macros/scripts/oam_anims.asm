2b948e0d1fb9ed36a70594048a4cdfd7e72252a5
Xylon <40894547+scriptorluminary@users.noreply.github.com>
2025-04-30 06:29:40 +0100

    Clarification

--- macros/scripts/oam_anims.asm
+++ macros/scripts/oam_anims.asm
@@ -4,7 +4,7 @@ MACRO oamframe
 	db \1 ; duration
 	DEF x = \2
 	assert !(x & (1 << (OAM_X_FLIP + 1) | 1 << (OAM_Y_FLIP + 1))), \
-		"oamframe duration overflows into X/Y flip bits"
+		"oamframe duration overflows into X/Y flip bits, owing to beginning negation"
 	if _NARG > 2
 		rept _NARG - 2
 			DEF x |= 1 << (\3 + 1)
